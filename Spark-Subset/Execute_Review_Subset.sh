ab6f94fb9278bcf1fd1392bd9147c39c9e3ff732
Ciaran Egan <63599288+i-am-yohan@users.noreply.github.com>
2020-07-23 21:11:14 +0100

    Add files via upload

--- Spark-Subset/Execute_Review_Subset.sh
+++ Spark-Subset/Execute_Review_Subset.sh
@@ -5,6 +5,12 @@ hdfs dfs -mkdir /Temp
 hdfs dfs -copyFromLocal $1 /Temp
 hdfs dfs -copyFromLocal $2 /Temp
 
+wget https://raw.githubusercontent.com/i-am-yohan/DMML2-yelp-project/master/Spark-Subset/Review_Subset.py
 python3 Review_Subset.py $1 $2
+rm Review_Subset.py
 
-hdfs dfs -get /Temp/Output $3
+hdfs dfs -get /Temp/Output Output
+rm Output/_SUCCESS
+mv Output/* $3
+
+hdfs dfs -rm -r /Temp

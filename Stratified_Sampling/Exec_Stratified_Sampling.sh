4911107124661badb9d0faf7cb4e8b02ec5f3658
Ciaran Egan <63599288+i-am-yohan@users.noreply.github.com>
2020-08-06 17:38:31 +0100

    Update Exec_Stratified_Sampling.sh

--- Stratified_Sampling/Exec_Stratified_Sampling.sh
+++ Stratified_Sampling/Exec_Stratified_Sampling.sh
@@ -5,8 +5,9 @@
 hdfs dfs -mkdir /Temp
 hdfs dfs -copyFromLocal $1 /Temp
 
-
-python3 /home/hduser/DMML2/Project/ETL/Stratified_Sampling/Stratified.py $1 $3
+wget https://raw.githubusercontent.com/i-am-yohan/DMML2-yelp-project/master/Stratified_Sampling/Stratified.py
+python3 Stratified.py $1 $3
+rm Stratified.py
 
 hdfs dfs -get /Temp/Output Output
 rm Output/_SUCCESS
